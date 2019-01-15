e923d0f757f4ee02e069b82c37c78b84fac41f11
Liu Bei <liubei1221@126.com>
2019-01-15 21:10:27 +0800

    Updates

---
+++ precache-manifest.435ced7a1e0660e23a501f8be96df70d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-admin/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "af3494c88faf756f175a",
+    "url": "/my-admin/static/js/runtime~main.af3494c8.js"
+  },
+  {
+    "revision": "3a1531cb12e5405770b5",
+    "url": "/my-admin/static/js/main.3a1531cb.chunk.js"
+  },
+  {
+    "revision": "52ad20a14de0dda9bb4a",
+    "url": "/my-admin/static/js/1.52ad20a1.chunk.js"
+  },
+  {
+    "revision": "3a1531cb12e5405770b5",
+    "url": "/my-admin/static/css/main.de558e38.chunk.css"
+  },
+  {
+    "revision": "52ad20a14de0dda9bb4a",
+    "url": "/my-admin/static/css/1.f3ab8d81.chunk.css"
+  },
+  {
+    "revision": "5003614b20174e1aedea269d83fe62c0",
+    "url": "/my-admin/index.html"
+  }
+];
